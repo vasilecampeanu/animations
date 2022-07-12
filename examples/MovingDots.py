7ee60e538be97b899867d4775128333cee4124fe
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-07-13 00:14:29 +0300

    feat: Samll fix.

--- examples/MovingDots.py
+++ examples/MovingDots.py
@@ -5,13 +5,16 @@ class MovingDots(Scene):
         d1, d2 = Dot(color = BLUE), Dot(color = GREEN)
         VGroup(d1, d2).arrange(RIGHT, buff = 1)
         l1 = Line(d1.get_center(), d2.get_center()).set_color(RED)
+
         x = ValueTracker(0)
         y = ValueTracker(0)
+        
         d1.add_updater(lambda z: z.set_x(x.get_value()))
         d2.add_updater(lambda z: z.set_y(y.get_value()))
         l1.add_updater(lambda z: z.become(Line(d1.get_center(), d2.get_center())))
+        
         self.add(d1, d2, l1)
         self.play(x.animate.set_value(5))
         self.play(y.animate.set_value(4))
+        
         return super().construct()
-    pass
